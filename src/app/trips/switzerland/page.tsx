c35c819e54e07c18d6f6f2769e2b4bc5d405cf22
suksencream <suksencream@gmail.com>
2025-07-10 12:42:53 +0700

    build

--- src/app/trips/switzerland/page.tsx
+++ src/app/trips/switzerland/page.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import Navbar from '@/app/components/Navbar'; 
 
 export default function SwitzerlandTripPage() {
